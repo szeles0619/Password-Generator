3ea5f91613cc851b5f3e3079af77a34291901196
Istvan <szeles0619@gmail.com>
2023-01-17 18:55:33 +0000

    Last changes added

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Password-Generator
+
+In this project I had to create a password generator using HTML, CSS and JavaScript. A user who visit this website they will have to enter a nummber between 10-64 and they will get the length of the password from the chosen number.
+
+Also they will be asked to choose the following: 
+
+    - Special characters.
+    - Numbers,
+    - Lowercase and Uppercase laters.
+
+
+## Deployed website: 
+
+https://szeles0619.github.io/Password-Generator/
+
+
+## Deployed website screenshot:
+
+<img width="957" alt="challenge5" src="https://user-images.githubusercontent.com/119215005/212986351-24655d41-8e8a-48aa-ad92-afcaed7b0272.png">
+
+
+## Usage
+
+As a user,
+I WANT to use the app,
+SO THAT I can generate passwords that fit my criteria
+GIVEN the password generator app.
+
+WHEN I click on the "generate password" button
+THEN I can see pop up windows where I can specify the password length and the types of characters I want to include
+
+WHEN I specify all the criteria,
+THEN I can see a corresponding password on the screen
+
+
+## Troubleshooting
+
+Keep it in mind that if you would like to create a new password you will have to refresh your web browser.
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+const specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+const lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+const upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -88,28 +88,78 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+// Declaration of global variables for the password option settings
+let characterLength = 0;
+let characterLengthPrompt
+let promptChoices = [];
+let passwordLength = 0;
 
+
+function getPasswordOptions() {
+  characterLengthPrompt = parseInt(prompt('How many characters do you want in your password? Please choose a number between 10 and 64'));
+  while (characterLengthPrompt !== null) {
+    // if the user inputs anumber below 10 or above 64 or any other caracter that is not a number the below message will be displayed.
+  if (isNaN(characterLengthPrompt) || characterLengthPrompt < 10 || characterLengthPrompt > 64) {
+    characterLengthPrompt = parseInt(prompt("Ooops, it looks like you entered an invalid number or letter. Please enter a number between 10 and 64 to choose the length of your password."));
+    // The else statement is used to specify if the statement above is false then it will go ahead and execute the code below it. The break statement breaks out of the loop and continues executing the code after the loop.
+  } else{
+    break;
+  }
 }
 
-// Function for getting a random element from an array
-function getRandom(arr) {
 
+if (confirm("Do you want to include special characters in your password?")) {
+  if (confirm){
+  promptChoices = promptChoices.concat(specialCharacters)
+}
+}
+
+if (confirm("Do you want to include numbers in your password?")) {
+  if (confirm){
+  promptChoices = promptChoices.concat(numericCharacters)
+}
+}
+
+if (confirm("Do you want to include lowercase letters in your password?")) {
+  if (confirm){
+  promptChoices = promptChoices.concat(lowerCasedCharacters)
+}
+}
+
+if (confirm("Do you want to include uppercase letters in your password?")) {
+  if (confirm){
+  promptChoices = promptChoices.concat(upperCasedCharacters)
+}
+}
+// If the user chooses to click cancel on all the confirm windows the following message will be displayed and the user has to start again.
+if (promptChoices.length === 0) {
+  alert("Ooops, you must choose at least one character type. Please try again.");
+}
+return;
+};
+
+// Function for getting a random element from an array
+function getRandom() {
+  return promptChoices[Math.floor(Math.random() * promptChoices.length)];
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  getPasswordOptions();
+  let displayPassword = ""
+  for (let i = 0; i < characterLengthPrompt; i++) {
+    displayPassword += getRandom();
+  }
+  return displayPassword;
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
